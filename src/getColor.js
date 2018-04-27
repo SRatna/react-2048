a4145dd61fea53c4f8be1b162904efe38c564328
sratna <adhikariratna52@gmail.com>
2018-04-27 12:04:21 +0545

    change colors

--- src/getColor.js
+++ src/getColor.js
@@ -3,20 +3,20 @@
  */
 const colors = {
   2 : '#eadfd4',
-  4 : '#cac7a0',
+  4 : '#dedbaf',
   8 : '#f2b179',
-  16 : '#f59563',
+  16 : '#f5c4b2',
   32 : '#cbb68a',
   64 : '#d3a0a0',
   128 : '#b9d7fa',
-  256 : '#676fcc',
-  512 : '#fa65b9',
-  1024 : '#5eb28c',
-  2048 : '#4394f0',
+  256 : '#98b2ec',
+  512 : '#fac1c4',
+  1024 : '#96ecc5',
+  2048 : '#c7f3ff',
   4096 : '#bcfa39',
-  8192 : '#6dfaac',
-  16384 : '#9d65fa',
-  32768 : '#14faed',
+  8192 : '#d2e3af',
+  16384 : '#c9c5f4',
+  32768 : '#b6cbfa',
 };
 const getColor = number => colors[number] || '#CDC1B5';
 export default getColor;
